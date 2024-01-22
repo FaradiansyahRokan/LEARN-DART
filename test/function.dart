afefe27de4d96d02583bd3a3104820cb320b187d
rokan <rokansubhi.f@gmail.com>
2024-01-22 16:00:04 +0530

    day2

--- test/function.dart
+++ test/function.dart
@@ -1,6 +1,22 @@
 void main() {
   var invest = tabungan(200000, 100000);
   print(invest);
+
+  // Memanggil fungsi dengan memberikan nilai untuk argumen opsional
+  greet("John");
+
+  // Memanggil fungsi tanpa memberikan nilai untuk argumen opsional
+  greet("Jane", greeting: "Hi", punctuation: "!");
+
+  // Memanggil fungsi dengan memberikan nilai untuk argumen opsional secara eksplisit
+  greet("Bob", greeting: "Hey", punctuation: "?");
+
+  greet("Atha", greeting: "Woi", punctuation: "!!!!!");
+}
+
+// Fungsi dengan argumen opsional dan nilai default
+void greet(String name, {String greeting = "Hello", String punctuation = "."}) {
+  print("$greeting $name$punctuation");
 }
 
 tabungan(porto1, porto2) {
