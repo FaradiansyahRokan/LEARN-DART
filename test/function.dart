3ef4db6b16d72a959c0db7e1e96b79ac87874875
rokan <rokansubhi.f@gmail.com>
2024-01-22 13:19:03 +0530

    day2

---
+++ test/function.dart
@@ -0,0 +1,10 @@
+void main() {
+  var invest = tabungan(200000, 100000);
+  print(invest);
+}
+
+tabungan(porto1, porto2) {
+  num totalPortofolio;
+  totalPortofolio = porto1 + porto2;
+  return totalPortofolio;
+}
