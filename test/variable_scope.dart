c5d831921bdbfe14029539e5db4ea32cb08f6605
rokan <rokansubhi.f@gmail.com>
2024-01-25 15:10:26 +0530

    day4

--- test/variable_scope.dart
+++ test/variable_scope.dart
@@ -1,9 +1,36 @@
 void main() {
+  // SCOPE
   var text;
   myFunct() {
-  text = "Hello";
-}
+    text = "Hello";
+  }
+
   myFunct();
   print(text);
-}
 
+  // CONST
+  // Penulisan Variable Biasa
+  var kota = "BANDUNG";
+  kota = "JAKARTA";
+  // penulisan Variable CONST
+  const negara = "INDIA";
+  // negara = "PAKISTAN";
+  // setiap varibale CONST dia tidak dapat di rubah atau perbarui seperti variable biasa
+
+  print(negara);
+  print(kota);
+
+// contoh penggunaan const
+  const int x = 5;
+  const double y = 3.14;
+  const List<int> numbers = [1, 2, 3];
+
+  print("Nilai x: $x");
+  print("Nilai y: $y");
+  print("List numbers: $numbers");
+
+
+  // FINAL
+
+
+}
