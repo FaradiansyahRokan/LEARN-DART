02058c6f73cd14fc18d087d15744de451187bc18
rokan <rokansubhi.f@gmail.com>
2024-01-26 10:18:06 +0530

    day4

---
+++ test/looping.dart
@@ -0,0 +1,27 @@
+void main() {
+  // for loop
+  for (int nilai = 0; nilai < 20; nilai++) {
+    print(nilai);
+  }
+
+  // while loop
+  int index = 0;
+  while (index < 15) {
+    print("$index");
+    index++;
+    }
+
+
+    // do while loop
+    int jumlah = 200;
+    do {
+      if (jumlah % 2 == 0) {
+        print("Angka $jumlah adalah genap.");
+        } else {
+          print("Angka $jumlah adalah ganjil.");
+          }
+          } 
+          while (jumlah <= 100);
+      
+
+}
