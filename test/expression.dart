3a7da1aeff928a3bc067045907524b5bccf3758a
rokan <rokansubhi.f@gmail.com>
2024-01-26 12:06:00 +0530

    day5

--- test/expression.dart
+++ test/expression.dart
@@ -1,13 +1,41 @@
 void main() {
-  // if else
-  int num = 200;
-  if (num > 50) {
-    print("KURANG BAGUSS");
-  } else if (num > 70) {
-    print("BAGUS");
-  } else if (num > 90) {
-    print("Sangat Bagus");
+  // Contoh if, else if, else
+  int angka = 5;
+
+  if (angka > 0) {
+    print("Angka positif");
+  } else if (angka < 0) {
+    print("Angka negatif");
   } else {
-    print("masih kureng");
+    print("Angka nol");
+  }
+
+  // Contoh switch case
+  String hari = "Selasa";
+
+  switch (hari) {
+    case "Senin":
+      print("Hari kerja");
+      break;
+    case "Selasa":
+      print("Hari kerja");
+      break;
+    case "Rabu":
+      print("Hari kerja");
+      break;
+    case "Kamis":
+      print("Hari kerja");
+      break;
+    case "Jumat":
+      print("Hari kerja");
+      break;
+    case "Sabtu":
+      print("Akhir pekan");
+      break;
+    case "Minggu":
+      print("Akhir pekan");
+      break;
+    default:
+      print("Hari tidak valid");
   }
 }
---
+++ test/oop/class.dart
@@ -0,0 +1,10 @@
+class Hewan {
+  late String nama;
+  late String umur;
+  late String berat;
+}
+
+void main() {
+  var manusia1 = Hewan();
+  print(manusia1.berat = "21");
+}
