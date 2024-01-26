c6ddaaa9659b0cc486cf9637390e81883d6ac934
rokan <rokansubhi.f@gmail.com>
2024-01-26 10:45:58 +0530

    day5

--- test/expression.dart
+++ test/expression.dart
@@ -1,10 +1,13 @@
 void main() {
   // if else
-  int num = 70;
+  int num = 200;
   if (num > 50) {
-    print("BAGUSSS");
+    print("KURANG BAGUSS");
+  } else if (num > 70) {
+    print("BAGUS");
+  } else if (num > 90) {
+    print("Sangat Bagus");
   } else {
     print("masih kureng");
   }
-  
 }
