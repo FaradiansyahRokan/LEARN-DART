2314ada86eb675215673213be39f1cc14355d9f7
rokan <rokansubhi.f@gmail.com>
2024-02-02 11:33:43 +0530

    day8

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.blue, //背景颜色
+        primarySwatch: Colors.blue, // Background color
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My App'),
+        ),
+        body: Column(
+          children:  [
+            Text("Hello Rokan"),
+            Text("Hello Rokan"),
+            Text("Hello Rokan"),
+            Text("Hello Rokan"),
+            Text("Hello Rokan"),
+            Text("Hello Rokan"),
+           Row(
+              children:  [
+                Expanded(
+                  child: Text("Hello Flutter"),
+                ),
+                Expanded(
+                  child: Text("Hello Flutter"),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
